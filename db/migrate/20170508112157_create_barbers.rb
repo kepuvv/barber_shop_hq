19d64ef722e15918caa8022d910d45644e706691
Kepuvv <renard.ru@gmail.com>
2017-05-08 14:25:59 +0300

    before migration

--- db/migrate/20170508112157_create_barbers.rb
+++ db/migrate/20170508112157_create_barbers.rb
@@ -1,4 +1,14 @@
 class CreateBarbers < ActiveRecord::Migration[5.1]
   def change
+  	create_table :barber do |t|
+  		t.text :name
+  		
+  		t.timestamps
+  	end
+
+  	Berber.create :name => 'Jessie Pinkman'
+  	Berber.create :name => 'Gus'
+  	Berber.create :name => 'Walter White'
+
   end
 end
