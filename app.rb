12c39948ae8dbf87960e902da0ccf594546978d6
Kepuvv <renard.ru@gmail.com>
2017-05-08 13:52:02 +0300

    add gitignore and create client table

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, "sqlite3:barbershop.rb"
+set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
 end 
