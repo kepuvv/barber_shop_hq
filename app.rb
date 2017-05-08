b89f9d443ebf1387b72017b59de41376a37c74c3
Kepuvv <renard.ru@gmail.com>
2017-05-08 16:11:14 +0300

    jquery dont work

--- app.rb
+++ app.rb
@@ -12,8 +12,63 @@ end
 class Barber < ActiveRecord::Base
 end  
 
-get '/' do
+before do
 	@barbers = Barber.all
+end
+
+get '/' do
+	@barbers
 	erb :index
 end
 
+get '/visit' do
+
+	@barbers
+
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@date = params[:datetime]
+	color = params[:color]
+	@barber = params[:barber]
+
+	db = get_db
+
+	@barbers = db.execute 'select * from barbers'
+
+	# автозаполнение введенных полей при повтороном вводе
+
+	hh = { 	:username => 'Введите имя', 
+			:phone => 'Введите телефон', 
+			:datetime => 'Выберите дату' }
+
+	# для каждой пары ключ-значение 
+	hh.each do |key, value|
+
+		if params[key] == ''
+			@error = value
+
+			return erb :visit 
+		end 
+	end
+
+	# Можно объединить сообщения об ошибках
+	# @error = hh.select { |key,_| params[key] == '' }.values.join(",")
+
+	db = get_db
+	db.execute 'insert into users 
+		(
+			name,
+			phone,
+			datestamp,
+			barber,
+			color
+		)
+		values (?,?,?,?,?)', [@username, @phone, @date, @barber, color]
+
+	erb "<h2>Спасибо! Вы записались #{@date}</h2>"
+	
+end
